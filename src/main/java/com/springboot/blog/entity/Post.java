fefe30171e86cdb9ef32d5b0942ae8f0cf308684
Manhong Yuan <Yuan.Manh@northeastern.edu>
2023-10-23 12:12:32 -0400

    created comment entity and create the relationship between comment and post

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,4 +30,17 @@ public class Post {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
+
 }
+
+/*
+CascadeType.ALL:
+当你保存（persist）一个Post实体时，相关的Comment实体也会被保存。
+当你更新（merge）一个Post实体时，相关的Comment实体也会被更新。
+当你删除（remove）一个Post实体时，相关的Comment实体也会被删除。
+
+orphanRemoval
+当 orphanRemoval 被设置为 false 时，从集合中删除一个 Comment 对象只会将其从集合中移除，而不会删除数据库中的相应行。这意味着在数据库中的 Comment 记录仍然存在，但不再与 Post 对象的 comments 集合相关联。
+ */
