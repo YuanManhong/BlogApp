1a69c98ce795231a53556809427f8b5ccdc5d06d
Manhong Yuan <Yuan.Manh@northeastern.edu>
2023-10-26 18:31:53 -0400

    built add category api

---
+++ src/main/java/com/springboot/blog/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Category;
+import com.springboot.blog.payload.CategoryDTO;
+import com.springboot.blog.repository.CategoryRepository;
+import com.springboot.blog.service.CategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryRepository categoryRepository;
+    private ModelMapper modelMapper;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper) {
+        this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public CategoryDTO addCategory(CategoryDTO categoryDTO) {
+        Category category = modelMapper.map(categoryDTO, Category.class);
+        Category savedCategory = categoryRepository.save(category);
+        return modelMapper.map(savedCategory, CategoryDTO.class);
+    }
+}
