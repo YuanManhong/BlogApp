e6ccd6cda8411c0d02cf6843b2b0174be5ecb980
Manhong Yuan <Yuan.Manh@northeastern.edu>
2023-10-26 19:43:20 -0400

    built get category by id

--- src/main/java/com/springboot/blog/controller/CategoryController.java
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -2,13 +2,12 @@ package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.CategoryDTO;
 import com.springboot.blog.service.CategoryService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -25,5 +24,11 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> addCategory(@RequestBody CategoryDTO categoryDTO){
         CategoryDTO savedCategory = categoryService.addCategory(categoryDTO);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryDTO> getCategory(@PathVariable("id") Long categoryId){
+        CategoryDTO categoryDTO = categoryService.getCategory(categoryId);
+        return ResponseEntity.ok(categoryDTO);
     }
 }
--- src/main/java/com/springboot/blog/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Category;
+import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.payload.CategoryDTO;
 import com.springboot.blog.repository.CategoryRepository;
 import com.springboot.blog.service.CategoryService;
@@ -22,5 +23,13 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = modelMapper.map(categoryDTO, Category.class);
         Category savedCategory = categoryRepository.save(category);
         return modelMapper.map(savedCategory, CategoryDTO.class);
+    }
+
+    @Override
+    public CategoryDTO getCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Category", "id", categoryId));
+        return modelMapper.map(category, CategoryDTO.class);
     }
 }
